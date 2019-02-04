2fa01205910e714253c6c2a42045a61801f0a503
szapp <dev.szapp@gmail.com>
2019-02-04 13:32:00 +0100

    Include legend and figure texts

--- __init__.py
+++ __init__.py
@@ -51,22 +51,36 @@ def _apply(obj, oldcolor, newcolor):
 
 def _savefig_new(self, fname, **kwargs):
     if getstate() == 'dark':
-        obj = []
+        pool = [self.get_children()]
         for ax in self.get_axes():
-            for c in (ax.get_children()
-                      + ax.xaxis.get_children()
-                      + ax.yaxis.get_children()
-                      + ax.xaxis.get_ticklines()
-                      + ax.yaxis.get_ticklines()
-                      + ax.xaxis.get_ticklabels()
-                      + ax.yaxis.get_ticklabels()):
-                if _apply(c, ['white', (1, 1, 1, 1)], 'black'):
-                    obj.append(c)
+            pool += [ax.get_children()
+                     + ax.xaxis.get_children()
+                     + ax.yaxis.get_children()
+                     + ax.xaxis.get_ticklines()
+                     + ax.yaxis.get_ticklines()
+                     + ax.xaxis.get_ticklabels()
+                     + ax.yaxis.get_ticklabels()]
+            if ax.get_legend():
+                if ax.get_legend().get_texts():
+                    pool += ax.get_legend().get_texts()
+                if ax.get_legend().get_frame():
+                    pool += [ax.get_legend().get_frame()]
+        obj = []
+        for c in pool:
+            if _apply(c, ['white',
+                          (1, 1, 1, 1),
+                          (0.8, 0.8, 0.8, 0.8)],  # Legend
+                      'black'):
+                obj.append(c)
 
-    _savefig_orig(self, fname, **kwargs)
-    if getstate() == 'dark':
+        _savefig_orig(self, fname, **kwargs)
+
         for o in obj:
-            _apply(o, ['black', (0, 0, 0, 1)], 'white')
+            if not _apply(o, ['black', (0, 0, 0, 1)], 'white'):
+                _apply(o, [(0, 0, 0, 0.8)], (0.8, 0.8, 0.8, 0.8))  # Legend
+    else:
+        # Light interface: No changes
+        _savefig_orig(self, fname, **kwargs)
 
 
 plt.Figure.savefig = _savefig_new
