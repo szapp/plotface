e3c8111407af9a78004ddf6cbe5a8f0fb1cf03c8
szapp <dev.szapp@gmail.com>
2019-05-22 11:17:04 +0200

    Simplify collecting all figure objects

--- __init__.py
+++ __init__.py
@@ -10,7 +10,6 @@ savefig function on loading to allow replacing the color of all white text and
 lines with black in case the dark interface is selected.
 """
 
-import matplotlib as mpl
 from matplotlib import pyplot as plt
 from .core import (
     getstate,
@@ -39,17 +38,6 @@ except NameError:
 def _apply(obj, oldcolor, newcolor):
     changed = False
 
-    # Recurse into sub objects
-    if isinstance(obj, (list,
-                        mpl.collections.LineCollection,
-                        mpl.lines.Line2D)):
-        if isinstance(obj, (mpl.collections.LineCollection, mpl.lines.Line2D)):
-            obj = obj.get_children()
-        for i in obj:
-            if _apply(i, oldcolor, newcolor):
-                changed = True
-        return changed
-
     # Fill color
     if hasattr(obj, 'get_color') and obj.get_color() in oldcolor:
         obj.set_color(newcolor)
@@ -67,35 +55,47 @@ def _apply(obj, oldcolor, newcolor):
             obj.set_edgecolor(newcolor)
             changed = True
 
+    # Marker colors
+    if (hasattr(obj, 'get_markerfacecolor')
+            and obj.get_markerfacecolor() in oldcolor):
+        obj.set_markerfacecolor(newcolor)
+        changed = True
+    if (hasattr(obj, 'get_markeredgecolor')
+            and obj.get_markeredgecolor() in oldcolor):
+        obj.set_markeredgecolor(newcolor)
+        changed = True
+
     return changed
 
 
 def _savefig_new(self, fname, **kwargs):
     if getstate() == 'dark':
-        pool = [self.get_children()]
-        for ax in self.get_axes():
-            pool += [ax.get_children()
-                     + ax.xaxis.get_children()
-                     + ax.yaxis.get_children()
-                     + ax.xaxis.get_ticklines()
-                     + ax.yaxis.get_ticklines()
-                     + ax.xaxis.get_ticklabels()
-                     + ax.yaxis.get_ticklabels()]
-            if ax.get_legend():
-                if ax.get_legend().get_texts():
-                    pool += ax.get_legend().get_texts()
-                if ax.get_legend().get_frame():
-                    pool += [ax.get_legend().get_frame()]
+        # Collect all objects from the figure
+        pool = self.get_children()
+
+        # Recurse into children
+        for ele in pool:
+            if hasattr(ele, 'get_children') and ele.get_children():
+                # pool.remove(ele)
+                pool += ele.get_children()
+            elif isinstance(ele, list):
+                pool.remove(ele)
+                pool += ele
+
+        # Iterate over all objects
         obj = []
-        for c in pool:
-            if _apply(c, ['white',
+        for c in set(pool):
+            if _apply(c, ['w', 'white',
                           (1, 1, 1, 1),
+                          (1, 1, 1, 0),
                           (0.8, 0.8, 0.8, 0.8)],  # Legend
                       'black'):
                 obj.append(c)
 
+        # Call original save function
         _savefig_orig(self, fname, **kwargs)
 
+        # Restore original colors
         for o in obj:
             if not _apply(o, ['black', (0, 0, 0, 1)], 'white'):
                 _apply(o, [(0, 0, 0, 0.8)], (0.8, 0.8, 0.8, 0.8))  # Legend
@@ -104,7 +104,11 @@ def _savefig_new(self, fname, **kwargs):
         _savefig_orig(self, fname, **kwargs)
 
 
-plt.Figure.savefig = _savefig_new
+def _load():
+    """
+    Apply the savefig hook
+    """
+    plt.Figure.savefig = _savefig_new
 
 
 def unload():
@@ -113,3 +117,7 @@ def unload():
     """
     plt.Figure.savefig = _savefig_orig
     light()
+
+
+# Apply hook on initialization
+_load()
